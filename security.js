58a527f748880fdbd2453630d0d17508301b7307
aurelie.calynsapp@gmail.com <aurelie.calynsapp@gmail.com>
2026-01-01 18:15:41 +0100

    V1.4.1

--- security.js
+++ security.js
@@ -1,34 +1,35 @@
 //security.js
 
-function startSecurityProcess() {			
-	var lang = localStorage.getItem('preferred-lang') || (navigator.language.startsWith('fr') ? 'fr' : 'en');
-	var t = translations[lang] || translations['fr']
-	var pass = prompt(t.prompt_password);
-	var secret = process.env.CHAT_PASSWORD;
+function startSecurityProcess() {            
+    var lang = localStorage.getItem('preferred-lang') || (navigator.language.startsWith('fr') ? 'fr' : 'en');
+    var t = translations[lang] || translations['fr'];
+    
+    var pass = prompt(t.prompt_password);
 
-	if (pass === secret) {
-		SECRET_KEY = prompt(t.prompt_secret_key);
-				
-		if (!SECRET_KEY) {
-			alert(t.alert_key_mandatory);
-			return;
-		}
+    socket = io();
 
-		myPseudo = prompt(t.prompt_pseudo) || "Anonyme";
+    socket.emit('check-auth', pass);
 
-		// --- C'EST ICI QU'ON LANCE LA CONNEXION ---
-		socket = io(); // On initialise la connexion seulement maintenant !
+    socket.on('auth-result', (response) => {
+        if (response.success) {
+            SECRET_KEY = prompt(t.prompt_secret_key); // La clé de chiffrement des messages
+            
+            if (!SECRET_KEY) {
+                alert(t.alert_key_mandatory);
+                return;
+            }
 
-		// On définit les écouteurs AVANT de faire le join
-		setupSocketListeners(); 
+            myPseudo = prompt(t.prompt_pseudo) || "Anonyme";
+            setupSocketListeners(); 
 
-		document.getElementById('welcome-screen').style.display = 'none';
-		document.getElementById('chat-container').style.display = 'flex';
+            document.getElementById('welcome-screen').style.display = 'none';
+            document.getElementById('chat-container').style.display = 'flex';
 
-		var myTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
-		socket.emit('join', myPseudo, myTZ);
-		
-	} else {
-		alert(t.alert_access_denied);
-	}
+            var myTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
+            socket.emit('join', myPseudo, myTZ);
+        } else {
+            alert(t.alert_access_denied);
+            socket.disconnect();
+        }
+    });
 }
